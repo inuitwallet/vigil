489eb5e323bbb2eee88757376f9d2efb16283782
MJ2P <mj2p.sg@gmail.com>
2018-05-08 14:20:35 +0100

    add tasks and Vigil Results to admin

--- vigil/admin.py
+++ vigil/admin.py
@@ -1,18 +1,31 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
 
-from vigil.models import AlertChannel, AlertAction, ChannelAction
+from vigil.models import VigilTaskResult, PreProcessorActionTask, Alert, NotificationActionTask, LogicActionTask
 
 
-@admin.register(AlertChannel)
-class AlertChannelAdmin(admin.ModelAdmin):
-    list_display = ['name', 'alert_id', 'active']
+@admin.register(VigilTaskResult)
+class VigilTaskResultAdmin(ModelAdmin):
+    list_display = ['task_id', 'alert_channel', 'alert_task_object', 'status']
+    raw_id_fields = ['alert_channel']
 
 
-@admin.register(AlertAction)
-class AlertActionAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'last_triggered', 'task']
+@admin.register(Alert)
+class AlertAdmin(ModelAdmin):
+    list_display = ['alert_channel', 'title', 'message', 'priority', 'active', 'alert_created', 'last_updated']
+    raw_id_fields = ['alert_channel']
 
 
-@admin.register(ChannelAction)
-class ChannelActionAdmin(admin.ModelAdmin):
-    list_display = ['priority', 'alert_channel', 'alert_action']
+@admin.register(PreProcessorActionTask)
+class PreProcessorActionTaskAdmin(ModelAdmin):
+    list_display = ['name', 'description', 'expected_data']
+
+
+@admin.register(NotificationActionTask)
+class NotificationActionTaskAdmin(ModelAdmin):
+    list_display = ['name', 'description', 'default_data']
+
+
+@admin.register(LogicActionTask)
+class LogicActionTaskAdmin(ModelAdmin):
+    list_display = ['name', 'description', 'expected_data', 'business_logic_data']
