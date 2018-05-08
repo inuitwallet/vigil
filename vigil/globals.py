cff0b0f6056d9ed474af0d5210e0c257d41c6017
MJ2P <mj2p.sg@gmail.com>
2018-05-08 14:21:51 +0100

    add colours to globals

--- vigil/globals.py
+++ vigil/globals.py
@@ -6,7 +6,16 @@ priorities = [
     ('Emergency', 'EMERGENCY')
 ]
 
+bootstrap_priorities = [
+    'table-default',
+    'table-success',
+    'table-info',
+    'table-warning',
+    'table-danger'
+]
+
 action_types = [
+    ('PreProcessor', 'PREPROCESSOR'),
     ('Notification', 'NOTIFICATION'),
     ('Logic', 'LOGIC')
 ]
